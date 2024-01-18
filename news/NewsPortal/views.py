e27fd3450d48c795c0d1993a2074a94844a55dfa
unknown <leva-555.876@mail.ru>
2024-01-18 19:46:23 +0300

    2nd part of project

--- news/NewsPortal/views.py
+++ news/NewsPortal/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 10
+    
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+
 
 # Create your views here.
